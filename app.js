b817bd76bb03e9960506867ee8dd296e93b1337e
hemanjalireddy <43489240+hemanjalireddy@users.noreply.github.com>
2022-02-04 19:28:00 +0530

    Add EJS layout feature

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 
 const app=express();
 
-let items=[];
-
+let items=["Buy Groceries"];
+let workList=[];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
+
 app.get("/",function(req,res){
     let today=new Date();
-  
     let day="";
     let options={
         weekday:"long",
@@ -21,13 +21,30 @@ app.get("/",function(req,res){
 
     day= today.toLocaleString("en-US",options);
 
-    res.render('list', {kindofDay: day, newListItems: items});
+    res.render('list', {listTitle: day, newListItems: items});
 })
   
 app.post("/",function(req,res){
     let item=req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.list==="Work"){
+        workList.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+ 
+})
+
+app.get("/work",function(req,res){
+    res.render('list',{listTitle:"Work List", newListItems:workList})
+})
+
+app.post("/work",function(req,res){
+    let work=req.body.newItem;
+    workList.push(work);
+    res.redirect("/work");
 })
 app.listen(3000,function() {
     console.log("app listening to port 3000");
